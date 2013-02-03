f5cd7346bbf34e87cd523092b8342ae5222271db
tom <tom@thelandons.com>
2013-02-03 12:16:20 -0800

    Stack traces

--- main.cpp
+++ main.cpp
@@ -4,7 +4,56 @@
 using namespace util;
 using namespace std;
 
+namespace {
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void print_trace() {
+    char pid_buf[30];
+    sprintf(pid_buf, "%d", getpid());
+    char name_buf[512];
+    name_buf[readlink("/proc/self/exe", name_buf, 511)]=0;
+    int child_pid = fork();
+    if (!child_pid) {
+        dup2(2,1); // redirect output to stderr
+        fprintf(stdout,"stack trace for %s pid=%s\n",name_buf,pid_buf);
+        execlp("gdb", "gdb", "--batch", "-n", "-ex", "thread", "-ex", "bt", name_buf, pid_buf, NULL);
+        printf("execlp failed");
+        abort(); /* If gdb failed to start */
+    } else {
+        waitpid(child_pid,NULL,0);
+    }
+}
+
+#include <execinfo.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_backtrace() {
+    void *array[10];
+    size_t size;
+    char **strings;
+    size_t i;
+
+    size = backtrace (array, 10);
+    strings = backtrace_symbols (array, size);
+
+    printf ("Obtained %zd stack frames.\n", size);
+
+    for (i = 0; i < size; i++)
+        printf ("%s\n", strings[i]);
+
+    free (strings);
+}
+
+}
+
 int main() {
-    cout << Id::generate() << endl;
+    //cout << Id::generate() << endl;
+    print_trace();
+    print_backtrace();
     return 0;
 }
