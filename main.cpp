e51595fb01a1de840c7d5f18abae2a7ec2b88118
tom <tom@thelandons.com>
2013-02-06 23:12:22 -0800

    Stack traces

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-#include "util/id.h"
+#include <cxxabi.h>
+#include <dlfcn.h>
+#include <execinfo.h>
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include "util/id.h"
 
 using namespace util;
 using namespace std;
@@ -30,26 +36,78 @@ void print_trace() {
 }
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <execinfo.h>
-#include <cxxabi.h>
-
 void print_backtrace() {
     void *array[10];
     size_t size;
     char **strings;
     size_t i;
 
-    size = backtrace (array, 10);
-    strings = backtrace_symbols (array, size);
+    size = backtrace(array, 10);
+    strings = backtrace_symbols(array, size);
 
-    printf ("Obtained %zd stack frames.\n", size);
+    //printf("Obtained %zd stack frames.\n", size);
 
     for (i = 0; i < size; i++)
-        printf ("%s\n", strings[i]);
+        printf("%s\n", strings[i]);
 
-    free (strings);
+    free(strings);
+    printf("\n");
+
+    using namespace abi;
+
+    /** Call-stack entry datastructure. */
+	struct entry {
+	    /** Default constructor that clears all fields. */
+	    entry () : line(0) {
+	    }
+
+	    std::string file;     ///< filename
+	    size_t      line;     ///< line number
+	    std::string function; ///< name of function or method
+
+	    /** Serialize entry into a text string. */
+	    std::string to_string () const {
+	        std::ostringstream os;
+	        os << file << " (" << line << "): " << function;
+	        return os.str();
+	    }
+	};
+
+    // retrieve call-stack
+    const int MAX_DEPTH = 20;
+    const int num_discard = -1;
+    void * trace[MAX_DEPTH];
+    int stack_depth = backtrace(trace, MAX_DEPTH);
+    std::vector<entry> stack;
+
+    for (int i = num_discard+1; i < stack_depth; i++) {
+        Dl_info dlinfo;
+        if(!dladdr(trace[i], &dlinfo))
+            continue;
+
+        const char * symname = dlinfo.dli_sname;
+
+        int    status;
+        char * demangled = abi::__cxa_demangle(symname, NULL, 0, &status);
+        if(status == 0 && demangled)
+            symname = demangled;
+
+        printf("entry: %s, %s\n", dlinfo.dli_fname, symname);
+
+        // store entry to stack
+        if (dlinfo.dli_fname && symname) {
+            entry e;
+            e.file     = dlinfo.dli_fname;
+            e.line     = 0; // unsupported
+            e.function = symname;
+            stack.push_back(e);
+        } else {
+            continue; // skip last entries below main
+        }
+
+        if (demangled)
+            free(demangled);
+    }
 }
 
 }
